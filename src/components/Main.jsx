04641d94fa9e4f2589a38493820664419bf08e66
gigiul <luigidalleaste@gmail.com>
2022-11-16 21:35:03 +0100

    finished Home and starting Row component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import requests from '../Request'
+import requests from '../Requests'
 
 const Main = () => {
     const [movies, setMovies] = useState([])
@@ -12,11 +12,32 @@ const Main = () => {
             setMovies(response.data.results)
         })
     }, [])
-    console.log(movie)
+   // console.log(movie)
+
+const truncateString = (str, num) => {
+    if (str?.length > num) {
+        return str.slice(0, num) + '...';
+    } else {
+        return str;
+        }
+   };
+
+
   return (
     <div className='w-full h-[550px] text-white'>
         <div className='w-full h-full>'>
-            <img className='w-full h-full object-fit' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+            <img className='w-full h-full object-cover' 
+            src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+            alt={movie?.title} />
+        <div className='absolute w-full top-[20%] p-4 md:p-8'>
+            <h1 className='text-4xl md:text-6xl font-bold'>{movie?.title || movie?.name || movie?.original_name}</h1>
+            <div className='my-4'>
+                <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+            </div>
+            <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
+            <p className='text-gray-200 w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[40%]'>{truncateString(movie?.overview, 100)}</p>
+        </div>
         </div>
     </div>
   )
