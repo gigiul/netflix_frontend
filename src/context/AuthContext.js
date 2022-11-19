548f32e589a6e2ecebbf3808d8cf9c1e80e36b59
gigiul <luigidalleaste@gmail.com>
2022-11-19 20:14:38 +0100

    finished signup and implemented log in with connection to firebase

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -41,6 +41,6 @@ export function AuthContextProvider({children}) {
     )
 }
 
-export function useAuth() {
+export function UserAuth() {
     return useContext(AuthContext);
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {UserAuth} from '../context/AuthContext'
 
 const Signup = () => {
+  const {user, signUp} = UserAuth()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      await signUp(email, password)
+      navigate('/')
+    } catch (error){
+      console.log(error)
+    }
+  }
   return (
     <>
       <div className='w-full h-screen'>
@@ -10,14 +26,24 @@ const Signup = () => {
             <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
               <div className='max-w-[320px] mx-auto py-16'>
                 <h1 className='text-3xl font-bold'>Sign Up</h1>
-                <form className='w-full flex flex-col py-4'>
-                  <input className='bg-gray-700 rounded my-2 p-3' type="email" placeholder='Email' autoComplete='email' />
-                  <input className='bg-gray-700 rounded my-2 p-3' type="password" placeholder='Password' autoComplete='current-password' />
+                <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                  className='bg-gray-700 rounded my-2 p-3' type="email" placeholder='Email' autoComplete='email' />
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                  className='bg-gray-700 rounded my-2 p-3' type="password" placeholder='Password' autoComplete='current-password' />
                   <button className='bg-red-600 text-white py-3 my-6 rounded font-bold'>Sign Up</button>
-                  <div className='flex justify-between items-center'>
+                  <div className='flex justify-between items-center text-sm text-gray-600'>
                     <p><input className='mr-2' type="checkbox"/>Remember me</p>
                     <p>Need Help?</p>
                   </div>
+                  <p className='py-8'>
+                  <span className='text-gray-600'>
+                    Already subscribed to Netflix?
+                  </span>{' '}
+                  <Link to='/login'>Sign In</Link>
+                </p>
                 </form>
               </div>
             </div>
