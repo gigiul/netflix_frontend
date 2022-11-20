317819fa44d98a2787734eca8018c116960fad49
gigiul <luigidalleaste@gmail.com>
2022-11-20 15:58:29 +0100

    Added protected routes, implemented Account page with liked movies and add a function that delete favourite movies from firebase

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,24 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import {auth} from '../firebase';
+import {auth, db} from '../firebase';
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged,
 } from 'firebase/auth';
+import {setDoc,doc} from 'firebase/firestore';
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({children}) {
     const [user, setUser] = useState({});
 
-    function signUp(email, password) {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
+   function signUp(email, password) {
+        createUserWithEmailAndPassword(auth, email, password);
+        setDoc(doc(db, 'users', email), {
+            savedShows: []
+        })
+      }
 
     function logOut() {
         return signOut(auth);
--- src/firebase.js
+++ src/firebase.js
@@ -22,4 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
